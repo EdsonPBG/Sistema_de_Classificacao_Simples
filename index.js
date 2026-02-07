2ddf546419bbe23076a57331beff9667868313f4
EdsonPBG <edsongamer27@gmail.com>
2026-02-06 21:47:25 -0300

    feat: Implementa funcionalidade de busca de alunos por ID

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const prompt = require('prompt-sync')();
 let alunos = [];
 let proximoId = 1;
 
+
 function validarNota (nota) {
     if (nota < 0 || nota > 10) {
         console.log("ERRO: Nota menor do que zero ou maior do que dez, tente novamente!");
@@ -38,6 +39,28 @@ function listaAlunos () {
     };
 };
 
+function buscarPorId () {
+    let id_aluno = Number(prompt("Informe o ID do aluno para a busca: "));
+    let existe = false;
+
+    if (alunos.length == 0) {
+            console.log("Não existem alunos cadastrados, por favor, cadastre!");
+            return;
+        }
+    else {
+        for (let i = 0; i < alunos.length; i++) {
+                if (id_aluno == alunos[i].id) { 
+                    existe = true;
+                    console.log(alunos[i]);
+                };
+        };  
+            if (existe == false) {
+                    console.log(`Aluno com o id: ${id_aluno} não encontrado! Tente novamente`);
+                    return;
+            };
+    };
+};
+
 function cadastrarAlunos () {
     let nome = String(prompt("Informe o nome do aluno: "));
     let nota = Number(prompt("Informe a nota do aluno (0 a 10): "));
@@ -47,7 +70,7 @@ function cadastrarAlunos () {
 
     let status = calcularStatus(nota);
 
-        const aluno = {
+     const aluno = {
             id: proximoId,
             name: nome,
             nota: nota,
@@ -66,6 +89,7 @@ console.log("-- Menu dos alunos --");
 console.log("");
 console.log("1. Cadastrar Aluno");
 console.log("2. Listar Alunos");
+console.log("3. Buscar Alunos Por ID");
 console.log("0. sair");
 console.log("");
     opcao = Number(prompt("Escolha um numero: "));
@@ -76,6 +100,9 @@ console.log("");
             break;
         case 2:
                 listaAlunos();
+            break;
+        case 3:
+                buscarPorId();
             break;
         case 0: 
                 console.log("Saindo...")
