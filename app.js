94aac64b2345ff801cc581a8ff1c2df688376951
EdsonPBG <edsongamer27@gmail.com>
2026-03-24 18:54:19 -0300

    feat: implementar rota POST /alunos para cadastro com persistencia

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.use(express.json())
 const { AlunosService } = require('./service/alunosService');
-const { carregar } = require('./service/repository');
+const { carregar, salvar } = require('./service/repository');
 
 carregar()
 app.get('/alunos', (req, res) => {
@@ -31,6 +32,21 @@ app.get('/alunos/:id', (req, res) => {
     {
         res.status(404).json({ menssagem: erro.message})
     }
+});
+
+app.post('/alunos', (req, res) => {
+    try 
+    {
+        const {nome, nota} = req.body
+        const cadastro = AlunosService.cadastrar(nome, nota)
+        salvar()
+        res.json(cadastro)
+        console.log(req.body)
+    }
+    catch (erro)
+    {
+        res.status(404).json({ menssagem: erro.message })
+    };
 });
 
 app.listen(port, () => {
